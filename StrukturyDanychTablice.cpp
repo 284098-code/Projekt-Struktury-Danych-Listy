d028bfc2832241b2f30360b869866b0a05a36fa5
284098-code <284098@student.pwr.edu.pl>
2026-04-21 20:51:20 +0200

    Update StrukturyDanychTablice.cpp

--- StrukturyDanychTablice.cpp
+++ StrukturyDanychTablice.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <chrono>
 #include <fstream>
 #include <cstdlib>
@@ -11,9 +11,8 @@
 using namespace std;
 using namespace std::chrono;
 
-const int TESTS = 50;
-const int OPS = 100;
-
+const int TESTS = 40;
+const int OPS = 80;
 
 void fill(Dynamiczna& td, Wiazana& l1, Dwokierunkowa& l2, int n) {
     for (int i = 0; i < n; i++) {
@@ -30,87 +29,148 @@ int main() {
     ofstream file("wyniki.csv");
     file << "N;TD_af;L1_af;L2_af;TD_ab;L1_ab;L2_ab;TD_find;L1_find;L2_find\n";
 
-    int sizes[] = { 1000, 5000, 10000, 20000, 50000 };
+    int sizes[] = { 1000, 2000, 5000, 10000, 20000 };
 
     for (int s = 0; s < 5; s++) {
         int N = sizes[s];
+
+        cout << "TEST DLA N = " << N << "\n";
 
         long long td_af = 0, l1_af = 0, l2_af = 0;
         long long td_ab = 0, l1_ab = 0, l2_ab = 0;
         long long td_fi = 0, l1_fi = 0, l2_fi = 0;
 
+        
+        Dynamiczna base_td;
+        Wiazana base_l1;
+        Dwokierunkowa base_l2;
+
+        cout << "Tworzenie bazy danych...\n";
+        fill(base_td, base_l1, base_l2, N);
+
         for (int t = 0; t < TESTS; t++) {
-
-            Dynamiczna td;
-            Wiazana l1;
-            Dwokierunkowa l2;
-
-            fill(td, l1, l2, N);
 
             int val = rand() % 10000;
 
             
-            auto start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) td.add_front(val);
-            auto end = high_resolution_clock::now();
-            td_af += duration_cast<nanoseconds>(end - start).count() / OPS;
+            cout << "add_front test...\n";
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l1.add_front(val);
-            end = high_resolution_clock::now();
-            l1_af += duration_cast<nanoseconds>(end - start).count() / OPS;
+            for (int i = 0; i < OPS; i++) {
+                Dynamiczna td = base_td;
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l2.add_front(val);
-            end = high_resolution_clock::now();
-            l2_af += duration_cast<nanoseconds>(end - start).count() / OPS;
+                auto start = high_resolution_clock::now();
+                td.add_front(val);
+                auto end = high_resolution_clock::now();
+
+                td_af += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Wiazana l1 = base_l1;
+
+                auto start = high_resolution_clock::now();
+                l1.add_front(val);
+                auto end = high_resolution_clock::now();
+
+                l1_af += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Dwokierunkowa l2 = base_l2;
+
+                auto start = high_resolution_clock::now();
+                l2.add_front(val);
+                auto end = high_resolution_clock::now();
+
+                l2_af += duration_cast<nanoseconds>(end - start).count();
+            }
 
             
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) td.add_back(val);
-            end = high_resolution_clock::now();
-            td_ab += duration_cast<nanoseconds>(end - start).count() / OPS;
+            cout << "add_back test...\n";
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l1.add_back(val);
-            end = high_resolution_clock::now();
-            l1_ab += duration_cast<nanoseconds>(end - start).count() / OPS;
+            for (int i = 0; i < OPS; i++) {
+                Dynamiczna td = base_td;
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l2.add_back(val);
-            end = high_resolution_clock::now();
-            l2_ab += duration_cast<nanoseconds>(end - start).count() / OPS;
+                auto start = high_resolution_clock::now();
+                td.add_back(val);
+                auto end = high_resolution_clock::now();
+
+                td_ab += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Wiazana l1 = base_l1;
+
+                auto start = high_resolution_clock::now();
+                l1.add_back(val);
+                auto end = high_resolution_clock::now();
+
+                l1_ab += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Dwokierunkowa l2 = base_l2;
+
+                auto start = high_resolution_clock::now();
+                l2.add_back(val);
+                auto end = high_resolution_clock::now();
+
+                l2_ab += duration_cast<nanoseconds>(end - start).count();
+            }
 
             
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) td.find(val);
-            end = high_resolution_clock::now();
-            td_fi += duration_cast<nanoseconds>(end - start).count() / OPS;
+            cout << "find test...\n";
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l1.find(val);
-            end = high_resolution_clock::now();
-            l1_fi += duration_cast<nanoseconds>(end - start).count() / OPS;
+            for (int i = 0; i < OPS; i++) {
+                Dynamiczna td = base_td;
 
-            start = high_resolution_clock::now();
-            for (int i = 0; i < OPS; i++) l2.find(val);
-            end = high_resolution_clock::now();
-            l2_fi += duration_cast<nanoseconds>(end - start).count() / OPS;
+                auto start = high_resolution_clock::now();
+                td.find(val);
+                auto end = high_resolution_clock::now();
+
+                td_fi += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Wiazana l1 = base_l1;
+
+                auto start = high_resolution_clock::now();
+                l1.find(val);
+                auto end = high_resolution_clock::now();
+
+                l1_fi += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+                Dwokierunkowa l2 = base_l2;
+
+                auto start = high_resolution_clock::now();
+                l2.find(val);
+                auto end = high_resolution_clock::now();
+
+                l2_fi += duration_cast<nanoseconds>(end - start).count();
+            }
         }
 
         
-        td_af /= TESTS; l1_af /= TESTS; l2_af /= TESTS;
-        td_ab /= TESTS; l1_ab /= TESTS; l2_ab /= TESTS;
-        td_fi /= TESTS; l1_fi /= TESTS; l2_fi /= TESTS;
+        td_af /= (TESTS * OPS);
+        l1_af /= (TESTS * OPS);
+        l2_af /= (TESTS * OPS);
 
-        
+        td_ab /= (TESTS * OPS);
+        l1_ab /= (TESTS * OPS);
+        l2_ab /= (TESTS * OPS);
+
+        td_fi /= (TESTS * OPS);
+        l1_fi /= (TESTS * OPS);
+        l2_fi /= (TESTS * OPS);
+
         file << N << ";"
             << td_af << ";" << l1_af << ";" << l2_af << ";"
             << td_ab << ";" << l1_ab << ";" << l2_ab << ";"
-            << td_fi << ";" << l1_fi << ";" << l2_fi << "\n";
+            << td_fi << ";" << l1_fi << ";" << l2_fi << endl;
 
-        
-        cout << "\nN = " << N << "\n";
+        cout << "\nWYNIKI:\n";
         cout << "add_front -> TD: " << td_af << " | L1: " << l1_af << " | L2: " << l2_af << "\n";
         cout << "add_back  -> TD: " << td_ab << " | L1: " << l1_ab << " | L2: " << l2_ab << "\n";
         cout << "find      -> TD: " << td_fi << " | L1: " << l1_fi << " | L2: " << l2_fi << "\n";
