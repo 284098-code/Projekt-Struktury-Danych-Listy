0805d60aa6ed2a574f9de10057bf73325322f801
284098-code <284098@student.pwr.edu.pl>
2026-05-17 17:16:08 +0200

    Add files via upload

--- StrukturyDanychTablice.cpp
+++ StrukturyDanychTablice.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+﻿#include <iostream>
 #include <chrono>
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
 
-#include "dynamiczna.h"
-#include "wiazana.h"
-#include "dwokierunkowa.h"
+#include "Dynamiczna.h"
+#include "Wiazana.h"
+#include "Dwokierunkowa.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -14,168 +14,430 @@ using namespace std::chrono;
 const int TESTS = 40;
 const int OPS = 80;
 
-void fill(Dynamiczna& td, Wiazana& l1, Dwokierunkowa& l2, int n) {
+// =========================================
+// KOLEJKA PRIORYTETOWA - DYNAMICZNA
+// =========================================
+
+class PriorityDynamiczna {
+private:
+    Dynamiczna dane;
+
+public:
+
+    void push(int value) {
+
+        int index = 0;
+
+        while (
+            index < dane.getsize() &&
+            dane[index] > value
+            ) {
+            index++;
+        }
+
+        dane.add_index(value, index);
+    }
+
+    void pop() {
+        dane.delete_front();
+    }
+
+    int peek() {
+
+        if (dane.getsize() == 0)
+            return -1;
+
+        return dane[0];
+    }
+
+    int size() {
+        return dane.getsize();
+    }
+
+    void modifyPriority(int oldValue, int newValue) {
+
+        int index = dane.find(oldValue);
+
+        if (index != -1) {
+
+            dane.delete_index(index);
+            push(newValue);
+        }
+    }
+};
+
+// =========================================
+// KOLEJKA PRIORYTETOWA - WIAZANA
+// =========================================
+
+class PriorityWiazana {
+private:
+    Wiazana dane;
+
+public:
+
+    void push(int value) {
+
+        int index = 0;
+
+        while (
+            index < dane.getsize() &&
+            dane.get(index) > value
+            ) {
+            index++;
+        }
+
+        dane.add_index(value, index);
+    }
+
+    void pop() {
+        dane.delete_front();
+    }
+
+    int peek() {
+
+        if (dane.getsize() == 0)
+            return -1;
+
+        return dane.get(0);
+    }
+
+    int size() {
+        return dane.getsize();
+    }
+
+    void modifyPriority(int oldValue, int newValue) {
+
+        int index = dane.find(oldValue);
+
+        if (index != -1) {
+
+            dane.delete_index(index);
+            push(newValue);
+        }
+    }
+};
+
+// =========================================
+// KOLEJKA PRIORYTETOWA - DWUKIERUNKOWA
+// =========================================
+
+class PriorityDwokierunkowa {
+private:
+    Dwokierunkowa dane;
+
+public:
+
+    void push(int value) {
+
+        int index = 0;
+
+        while (
+            index < dane.getsize() &&
+            dane.get(index) > value
+            ) {
+            index++;
+        }
+
+        dane.add_index(value, index);
+    }
+
+    void pop() {
+        dane.delete_front();
+    }
+
+    int peek() {
+
+        if (dane.getsize() == 0)
+            return -1;
+
+        return dane.get(0);
+    }
+
+    int size() {
+        return dane.getsize();
+    }
+
+    void modifyPriority(int oldValue, int newValue) {
+
+        int index = dane.find(oldValue);
+
+        if (index != -1) {
+
+            dane.delete_index(index);
+            push(newValue);
+        }
+    }
+};
+
+// =========================================
+// WYPEŁNIANIE STRUKTUR
+// =========================================
+
+void fill(
+    PriorityDynamiczna& td,
+    PriorityWiazana& l1,
+    PriorityDwokierunkowa& l2,
+    int n
+) {
+
     for (int i = 0; i < n; i++) {
+
         int val = rand() % 10000;
-        td.add_back(val);
-        l1.add_back(val);
-        l2.add_back(val);
+
+        td.push(val);
+        l1.push(val);
+        l2.push(val);
     }
 }
 
+// =========================================
+// MAIN
+// =========================================
+
 int main() {
+
     srand(time(NULL));
 
-    ofstream file("wyniki.csv");
-    file << "N;TD_af;L1_af;L2_af;TD_ab;L1_ab;L2_ab;TD_find;L1_find;L2_find\n";
+    ofstream file("wyniki_kolejki.csv");
+
+    file << "N;"
+        << "TD_push;L1_push;L2_push;"
+        << "TD_pop;L1_pop;L2_pop;"
+        << "TD_modify;L1_modify;L2_modify\n";
 
     int sizes[] = { 1000, 2000, 5000, 10000, 20000 };
 
     for (int s = 0; s < 5; s++) {
+
         int N = sizes[s];
 
-        cout << "TEST DLA N = " << N << "\n";
+        cout << "\n====================================\n";
+        cout << "TEST DLA N = " << N << endl;
+        cout << "====================================\n";
 
-        long long td_af = 0, l1_af = 0, l2_af = 0;
-        long long td_ab = 0, l1_ab = 0, l2_ab = 0;
-        long long td_fi = 0, l1_fi = 0, l2_fi = 0;
+        long long td_push = 0;
+        long long l1_push = 0;
+        long long l2_push = 0;
 
-        
-        Dynamiczna base_td;
-        Wiazana base_l1;
-        Dwokierunkowa base_l2;
+        long long td_pop = 0;
+        long long l1_pop = 0;
+        long long l2_pop = 0;
 
-        cout << "Tworzenie bazy danych...\n";
+        long long td_mod = 0;
+        long long l1_mod = 0;
+        long long l2_mod = 0;
+
+        // =====================================
+        // BAZOWE STRUKTURY
+        // =====================================
+
+        PriorityDynamiczna base_td;
+        PriorityWiazana base_l1;
+        PriorityDwokierunkowa base_l2;
+
+        cout << "Tworzenie struktur..." << endl;
+
         fill(base_td, base_l1, base_l2, N);
+
+        // =====================================
+        // TESTY
+        // =====================================
 
         for (int t = 0; t < TESTS; t++) {
 
             int val = rand() % 10000;
 
-            
-            cout << "add_front test...\n";
+            // =================================
+            // PUSH
+            // =================================
+
+            cout << "push test..." << endl;
 
             for (int i = 0; i < OPS; i++) {
-                Dynamiczna td = base_td;
+
+                PriorityDynamiczna q = base_td;
 
                 auto start = high_resolution_clock::now();
-                td.add_front(val);
+
+                q.push(val);
+
                 auto end = high_resolution_clock::now();
 
-                td_af += duration_cast<nanoseconds>(end - start).count();
+                td_push += duration_cast<nanoseconds>(end - start).count();
             }
 
             for (int i = 0; i < OPS; i++) {
-                Wiazana l1 = base_l1;
+
+                PriorityWiazana q = base_l1;
 
                 auto start = high_resolution_clock::now();
-                l1.add_front(val);
+
+                q.push(val);
+
                 auto end = high_resolution_clock::now();
 
-                l1_af += duration_cast<nanoseconds>(end - start).count();
+                l1_push += duration_cast<nanoseconds>(end - start).count();
             }
 
             for (int i = 0; i < OPS; i++) {
-                Dwokierunkowa l2 = base_l2;
+
+                PriorityDwokierunkowa q = base_l2;
 
                 auto start = high_resolution_clock::now();
-                l2.add_front(val);
+
+                q.push(val);
+
                 auto end = high_resolution_clock::now();
 
-                l2_af += duration_cast<nanoseconds>(end - start).count();
+                l2_push += duration_cast<nanoseconds>(end - start).count();
             }
 
-            
-            cout << "add_back test...\n";
+            // =================================
+            // POP
+            // =================================
+
+            cout << "pop test..." << endl;
 
             for (int i = 0; i < OPS; i++) {
-                Dynamiczna td = base_td;
+
+                PriorityDynamiczna q = base_td;
 
                 auto start = high_resolution_clock::now();
-                td.add_back(val);
+
+                q.pop();
+
                 auto end = high_resolution_clock::now();
 
-                td_ab += duration_cast<nanoseconds>(end - start).count();
-            }
-
-            for (int i = 0; i < OPS; i++) {
-                Wiazana l1 = base_l1;
-
-                auto start = high_resolution_clock::now();
-                l1.add_back(val);
-                auto end = high_resolution_clock::now();
-
-                l1_ab += duration_cast<nanoseconds>(end - start).count();
-            }
-
-            for (int i = 0; i < OPS; i++) {
-                Dwokierunkowa l2 = base_l2;
-
-                auto start = high_resolution_clock::now();
-                l2.add_back(val);
-                auto end = high_resolution_clock::now();
-
-                l2_ab += duration_cast<nanoseconds>(end - start).count();
-            }
-
-            
-            cout << "find test...\n";
-
-            for (int i = 0; i < OPS; i++) {
-                Dynamiczna td = base_td;
-
-                auto start = high_resolution_clock::now();
-                td.find(val);
-                auto end = high_resolution_clock::now();
-
-                td_fi += duration_cast<nanoseconds>(end - start).count();
+                td_pop += duration_cast<nanoseconds>(end - start).count();
             }
 
             for (int i = 0; i < OPS; i++) {
-                Wiazana l1 = base_l1;
+
+                PriorityWiazana q = base_l1;
 
                 auto start = high_resolution_clock::now();
-                l1.find(val);
+
+                q.pop();
+
                 auto end = high_resolution_clock::now();
 
-                l1_fi += duration_cast<nanoseconds>(end - start).count();
+                l1_pop += duration_cast<nanoseconds>(end - start).count();
             }
 
             for (int i = 0; i < OPS; i++) {
-                Dwokierunkowa l2 = base_l2;
+
+                PriorityDwokierunkowa q = base_l2;
 
                 auto start = high_resolution_clock::now();
-                l2.find(val);
+
+                q.pop();
+
                 auto end = high_resolution_clock::now();
 
-                l2_fi += duration_cast<nanoseconds>(end - start).count();
+                l2_pop += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            // =================================
+            // MODIFY PRIORITY
+            // =================================
+
+            cout << "modify test..." << endl;
+
+            for (int i = 0; i < OPS; i++) {
+
+                PriorityDynamiczna q = base_td;
+
+                auto start = high_resolution_clock::now();
+
+                q.modifyPriority(q.peek(), rand() % 10000);
+
+                auto end = high_resolution_clock::now();
+
+                td_mod += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+
+                PriorityWiazana q = base_l1;
+
+                auto start = high_resolution_clock::now();
+
+                q.modifyPriority(q.peek(), rand() % 10000);
+
+                auto end = high_resolution_clock::now();
+
+                l1_mod += duration_cast<nanoseconds>(end - start).count();
+            }
+
+            for (int i = 0; i < OPS; i++) {
+
+                PriorityDwokierunkowa q = base_l2;
+
+                auto start = high_resolution_clock::now();
+
+                q.modifyPriority(q.peek(), rand() % 10000);
+
+                auto end = high_resolution_clock::now();
+
+                l2_mod += duration_cast<nanoseconds>(end - start).count();
             }
         }
 
-        
-        td_af /= (TESTS * OPS);
-        l1_af /= (TESTS * OPS);
-        l2_af /= (TESTS * OPS);
+        // =====================================
+        // ŚREDNIE CZASY
+        // =====================================
 
-        td_ab /= (TESTS * OPS);
-        l1_ab /= (TESTS * OPS);
-        l2_ab /= (TESTS * OPS);
+        td_push /= (TESTS * OPS);
+        l1_push /= (TESTS * OPS);
+        l2_push /= (TESTS * OPS);
 
-        td_fi /= (TESTS * OPS);
-        l1_fi /= (TESTS * OPS);
-        l2_fi /= (TESTS * OPS);
+        td_pop /= (TESTS * OPS);
+        l1_pop /= (TESTS * OPS);
+        l2_pop /= (TESTS * OPS);
+
+        td_mod /= (TESTS * OPS);
+        l1_mod /= (TESTS * OPS);
+        l2_mod /= (TESTS * OPS);
+
+        // =====================================
+        // ZAPIS CSV
+        // =====================================
 
         file << N << ";"
-            << td_af << ";" << l1_af << ";" << l2_af << ";"
-            << td_ab << ";" << l1_ab << ";" << l2_ab << ";"
-            << td_fi << ";" << l1_fi << ";" << l2_fi << endl;
+            << td_push << ";" << l1_push << ";" << l2_push << ";"
+            << td_pop << ";" << l1_pop << ";" << l2_pop << ";"
+            << td_mod << ";" << l1_mod << ";" << l2_mod
+            << endl;
 
-        cout << "\nWYNIKI:\n";
-        cout << "add_front -> TD: " << td_af << " | L1: " << l1_af << " | L2: " << l2_af << "\n";
-        cout << "add_back  -> TD: " << td_ab << " | L1: " << l1_ab << " | L2: " << l2_ab << "\n";
-        cout << "find      -> TD: " << td_fi << " | L1: " << l1_fi << " | L2: " << l2_fi << "\n";
+        // =====================================
+        // WYNIKI
+        // =====================================
+
+        cout << "\nWYNIKI:\n" << endl;
+
+        cout << "push   -> "
+            << "TD: " << td_push
+            << " | L1: " << l1_push
+            << " | L2: " << l2_push
+            << endl;
+
+        cout << "pop    -> "
+            << "TD: " << td_pop
+            << " | L1: " << l1_pop
+            << " | L2: " << l2_pop
+            << endl;
+
+        cout << "modify -> "
+            << "TD: " << td_mod
+            << " | L1: " << l1_mod
+            << " | L2: " << l2_mod
+            << endl;
     }
 
     file.close();
+
+    cout << "\nZapisano wyniki do pliku wyniki_kolejki.csv\n";
+
     return 0;
 }
